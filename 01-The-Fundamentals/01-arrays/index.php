ec6cbdeb1df623c5df7c28fb7ecde69a7bbc2bef
odmrs <omarcosviniciusdev@gmail.com>
2024-02-27 17:05:01 -0300

    feat: add how use arrays

--- 01-The-Fundamentals/01-arrays/index.php
+++ 01-The-Fundamentals/01-arrays/index.php
@@ -6,6 +6,37 @@
     <title>Arrays</title>
 </head>
 <body>
-    
+    <?= 
+        $books = [
+            "Androids crazys",
+            "The Langoliers",
+            "Hail Mary"
+        ];
+    ?>
+    <h1>Recommended Books</h1>
+
+    <ul>
+        <?php foreach ($books as $book) {
+            echo "<li>$book</li>";
+        }
+        ?>
+    </ul>
+
+    <h1> HomeWork</h1>
+
+    <h2>Top Performing Users</h2>
+    <ul>
+        <?php
+            $stopPerformingUsers = [
+                "Marcos",
+                "Bela",
+                "Anna"
+            ];
+
+            foreach ($stopPerformingUsers as $user){
+                echo "<li>$user</li>";
+            }
+        ?>
+    </ul>
 </body>
 </html>
