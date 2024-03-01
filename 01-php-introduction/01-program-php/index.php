74c517c76abc20ff7c7082bab258dd691b379ca6
odmrs <omarcosviniciusdev@gmail.com>
2024-03-01 17:39:44 -0300

    feat: add lecture about variables

---
+++ 01-php-introduction/01-program-php/index.php
@@ -0,0 +1,65 @@
+<?php
+// Commenters;
+# Others commenters;
+/* 
+    dwakodka
+    d
+    awd
+    ad
+    aw
+    d
+*/
+
+// Output
+// echo 'Hello' . '<br>' . PHP_EOL;
+// echo 'World' . '<br>' . PHP_EOL;
+
+// print 'ABC' . '<br>' . PHP_EOL;
+
+// $vector = array('Marcos', 'Gol', 'Fiesta', 'Corsa');
+
+// var_dump($vector);  // Informations with details about a variable
+
+
+// Variables start with '$'
+
+// $first_name = 'Marcos';
+// $second_name = 'Vinicius';
+
+// echo "$first_name $second_name" . PHP_EOL;
+
+// $variable = 'nome';
+
+// $$variable = "Maria";
+
+// echo $nome;
+
+// $a = 5;
+// $b = $a;
+// $b = 10;
+
+// echo $a . PHP_EOL;
+// echo $b;
+
+// Using pointers | Objects are all ways copied by references
+
+// $a = 5;
+// $b = &$a;
+// $b = 10;
+
+// echo $a;
+// echo " ";
+// echo $b;
+
+// $a = new stdClass(); // Create a object
+
+// $a->name = "Maria"; // Define a attribute
+// $b = $a; // Make a copy
+// $b->name = "Joana"; // Replace the attribute
+// print $a->name;
+
+// echo ' ';
+
+// print $b->name;
+
+// BOOLEANS
