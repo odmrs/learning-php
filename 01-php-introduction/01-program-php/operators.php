f858a2c4bf5e7f541347284d9ff427e4ac61fb5c
odmrs <omarcosviniciusdev@gmail.com>
2024-03-05 23:05:20 -0300

    feat: learn about conditional statement, loops, and loops in array's

--- 01-php-introduction/01-program-php/operators.php
+++ 01-php-introduction/01-program-php/operators.php
@@ -9,13 +9,56 @@
 //    echo "Incompatible types";
 //}
 
-$will_raining = TRUE;
-$is_cold = FALSE;
+// $will_raining = TRUE;
+// $is_cold = FALSE;
 
-if ($will_raining and $is_cold) {
-    echo "Não vou sair de casa";
-}
+// if ($will_raining and $is_cold) {
+//     echo "Não vou sair de casa";
+// }
 
-if ($will_raining xor $is_cold) {
-    echo "Vou pensar duas vezes";
+// if ($will_raining xor $is_cold) {
+//     echo "Vou pensar duas vezes";
+// }
+
+// var_dump(__FILE__);
+// var_dump(__DIR__);
+// var_dump(__LINE__);
+
+// $a = '';
+
+// if ($a) {
+//     echo "A have something";
+// } else if ($b) { // Will give a warning
+//     echo "Something";
+// }
+
+// for ($i = 0; $i <= 10; $i++) {
+//     echo $i . PHP_EOL;
+// }
+// $a = 10;
+
+// switch ($a) {
+//     case 5:
+//         // Comands
+//         echo "Number 5" . PHP_EOL;
+//         break;
+//     case 3:
+//         // Comands
+//         echo "Number 3" . PHP_EOL;
+//         break;
+//     case 2:
+//         // Comands
+//         echo "Number 2" . PHP_EOL;
+//         break;
+//     default:
+//         echo "10" . PHP_EOL;
+// }
+
+
+// foreach
+
+$fruits = ['Apple', 'Banana', 'Watermelon'];
+
+foreach ($fruits as $fruit) {
+    echo $fruit . PHP_EOL;
 }
